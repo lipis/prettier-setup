f2ee8d852f2ba7f8d19ea0c55421973a8a530cdc
Michael Koppen <michael.koppen@googlemail.com>
2017-06-13 01:19:44 +0200

    es5-dangle

--- foo.js
+++ foo.js
@@ -2,6 +2,21 @@
 
 module.exports = foo => {
   foo.registerTask('bar', () =>
-    foo.task.run(['task:open_bar', 'task:close_bar']),
+    foo.task.run(['task:open_bar', 'task:close_bar'])
   );
+  const test = [
+    'lala',
+    'lala',
+    'lolo',
+    'lele',
+    'lala',
+    'lala',
+    'lolo',
+    'lele',
+    'lala',
+    'lala',
+    'lolo',
+    'lele',
+  ];
+  test.lala;
 };
